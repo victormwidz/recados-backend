dc9244314c4ef8d753c6c7dc82400c9f561c937a
victor <victormwidz@gmail.com>
2022-03-20 18:23:00 -0300

    alteracao

--- src/index.ts
+++ src/index.ts
@@ -2,7 +2,7 @@ import express, { Request, response, Response, RequestHandler } from 'express';
 import cors from 'cors';
 
 const app = express();
-const port = process.env.PORT || 8080
+const port = process.env.PORT || 8080;
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
